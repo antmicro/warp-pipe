0aa0019f48c59ebc8481d04e3184a5a8cb7e3b7e
Filip Kokosinski <fkokosinski@antmicro.com>
2023-10-27 11:57:20 +0200

    [#50478] inc/pcie_comm: add include guards to proto.h

--- inc/pcie_comm/proto.h
+++ inc/pcie_comm/proto.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2023 Antmicro <www.antmicro.com>
+ * Copyright 2023 Meta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PCIE_COMM_PROTO_H
+#define PCIE_COMM_PROTO_H
+
 #include <bits/endian.h>
 #include <assert.h>
 #include <stdint.h>
@@ -224,3 +244,5 @@ struct pcie_transport {
 static_assert(sizeof(struct pcie_dllp) == 6);
 static_assert(sizeof(struct pcie_tlp) == 16);
 static_assert(sizeof(struct pcie_transport) == 19);
+
+#endif /* PCIE_COMM_PROTO_H */
