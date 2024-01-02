7d9b5455d788f6508f446384e3900db408df003a
Franciszek Zdobylak <fzdobylak@antmicro.com>
2024-01-02 08:26:39 +0100

    memory-mock: Organize definitions
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- memory-mock/main.c
+++ memory-mock/main.c
@@ -31,41 +31,6 @@
 #include <warppipe/config.h>
 #include <warppipe/proto.h>
 
-static struct warppipe_server_t server = {
-	.listen = true,
-	.addr_family = AF_UNSPEC,
-	.host = NULL,
-	.port = SERVER_PORT_NUM,
-	.quit = false,
-};
-
-static void handle_sigint(int signo)
-{
-	server.quit = true;
-	syslog(LOG_NOTICE, "Received SIGINT. " PRJ_NAME_LONG " will shut down.");
-
-	/* disconnect every user */
-	warppipe_server_disconnect_clients(&server, NULL);
-}
-
-/* XXX: temporary PCIe configuration space structure */
-struct pcie_configuration_space_header {
-	uint16_t	vendor_id;
-	uint16_t	device_id;
-	uint8_t		__unused[10];
-	uint8_t		header_type;
-	uint8_t		__unused_bist;
-	uint32_t	bar[6];
-};
-
-struct bar_config_t {
-	uint8_t config;
-	uint32_t size;
-	warppipe_read_cb_t read_cb;
-	warppipe_write_cb_t write_cb;
-	void *data;
-};
-
 #define BAR_INACTIVE 0
 
 #define BAR_N 6
@@ -89,32 +54,24 @@ struct bar_config_t {
 #define BAR_TYPE_64B (0x10 << BAR_TYPE_OFFSET)
 #define BAR_MEMORY_SPACE 0x1
 
-int read_bar(struct bar_config_t bar, uint64_t addr, void *data, int length, void *private_data)
-{
-	if (addr > bar.size)
-		return 1;
+/* XXX: temporary PCIe configuration space structure */
+struct pcie_configuration_space_header {
+	uint16_t	vendor_id;
+	uint16_t	device_id;
+	uint8_t		__unused[10];
+	uint8_t		header_type;
+	uint8_t		__unused_bist;
+	uint32_t	bar[6];
+};
 
-	int max_len = bar.size - addr;
+struct bar_config_t {
+	uint8_t config;
+	uint32_t size;
+	warppipe_read_cb_t read_cb;
+	warppipe_write_cb_t write_cb;
+	void *data;
+};
 
-	if (length > max_len)
-		length = max_len;
-
-	memcpy(data, (uint8_t *)bar.data + addr, length);
-	return 0;
-}
-
-void write_bar(struct bar_config_t bar, uint64_t addr, const void *data, int length, void *private_data)
-{
-	if (addr > bar.size)
-		return;
-
-	int max_len = bar.size - addr;
-
-	if (length > max_len)
-		length = max_len;
-
-	memcpy((uint8_t *)bar.data + addr, data, length);
-}
 
 #define DECLARE_BAR_READ_CB(idx) \
 	static int __read_bar_cb_##idx(uint64_t addr, void *data, int length, void *private_data)
@@ -140,8 +97,7 @@ DECLARE_BAR_READ_CB(0);
 DECLARE_BAR_READ_CB(1);
 DECLARE_BAR_WRITE_CB(1);
 
-#define BAR0_SIZE 128
-static int8_t bar0_memory[BAR0_SIZE] = {
+static int8_t bar0_memory[128] = {
 	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
 	0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
 	0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
@@ -152,20 +108,25 @@ static int8_t bar0_memory[BAR0_SIZE] = {
 	0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f,
 };
 
-#define BAR1_SIZE 1024
-static int8_t bar1_memory[BAR1_SIZE] = {};
+static int8_t bar1_memory[1024] = {};
+
+static struct pcie_configuration_space_header configuration_space = {
+	.vendor_id = 0x1,
+	.device_id = 0x2,
+	.header_type = 0x0,
+};
 
 static struct bar_config_t bars_config[6] = {
 	[0] = {
 		.config = BAR_TYPE_32B | BAR_MEMORY_SPACE,
-		.size = BAR0_SIZE,
+		.size = sizeof(bar0_memory),
 		.read_cb = REF_BAR_CB(read, 0),
 		.write_cb = NULL,
 		.data = bar0_memory,
 	},
 	[1] = {
 		.config = BAR_TYPE_32B | BAR_MEMORY_SPACE,
-		.size = BAR1_SIZE,
+		.size = sizeof(bar1_memory),
 		.read_cb = REF_BAR_CB(read, 1),
 		.write_cb = REF_BAR_CB(write, 1),
 		.data = bar1_memory,
@@ -176,16 +137,53 @@ static struct bar_config_t bars_config[6] = {
 	[5] = { .config = BAR_INACTIVE, },
 };
 
+static struct warppipe_server_t server = {
+	.listen = true,
+	.addr_family = AF_UNSPEC,
+	.host = NULL,
+	.port = SERVER_PORT_NUM,
+	.quit = false,
+};
+
+static void handle_sigint(int signo)
+{
+	server.quit = true;
+	syslog(LOG_NOTICE, "Received SIGINT. " PRJ_NAME_LONG " will shut down.");
+
+	/* disconnect every user */
+	warppipe_server_disconnect_clients(&server, NULL);
+}
+
+static int read_bar(struct bar_config_t bar, uint64_t addr, void *data, int length, void *private_data)
+{
+	if (addr > bar.size)
+		return 1;
+
+	int max_len = bar.size - addr;
+
+	if (length > max_len)
+		length = max_len;
+
+	memcpy(data, (uint8_t *)bar.data + addr, length);
+	return 0;
+}
+
+static void write_bar(struct bar_config_t bar, uint64_t addr, const void *data, int length, void *private_data)
+{
+	if (addr > bar.size)
+		return;
+
+	int max_len = bar.size - addr;
+
+	if (length > max_len)
+		length = max_len;
+
+	memcpy((uint8_t *)bar.data + addr, data, length);
+}
 /* Definitions of the BARs callbacks. */
 DEFINE_BAR_READ_CB(0)
 DEFINE_BAR_READ_CB(1)
 DEFINE_BAR_WRITE_CB(1)
-
-static struct pcie_configuration_space_header configuration_space = {
-	.vendor_id = 0x1,
-	.device_id = 0x2,
-	.header_type = 0x0,
-};
 
 static struct warppipe_client_t *mock_dev_client;
 
@@ -196,7 +194,7 @@ static struct warppipe_client_t *mock_dev_client;
 	(((addr) - offsetof(struct pcie_configuration_space_header, bar)) / sizeof(uint32_t))
 
 
-void handle_bar_write(int bar_idx, uint32_t value, int length)
+static void handle_bar_write(int bar_idx, uint32_t value, int length)
 {
 	struct bar_config_t bar = bars_config[bar_idx];
 	uint32_t bar_addr = value & ~BAR_MASK_SIZE(bar.size);
@@ -210,7 +208,7 @@ void handle_bar_write(int bar_idx, uint32_t value, int length)
 	}
 }
 
-int config0_read_cb(uint64_t addr, void *data, int length, void *private_data)
+static int config0_read_cb(uint64_t addr, void *data, int length, void *private_data)
 {
 	// XXX: only 4B aligned accesses are allowed here
 	if ((addr & 0x3) || (length != 4)) {
@@ -226,7 +224,7 @@ int config0_read_cb(uint64_t addr, void *data, int length, void *private_data)
 	return 0;
 }
 
-void config0_write_cb(uint64_t addr, const void *data, int length, void *private_data)
+static void config0_write_cb(uint64_t addr, const void *data, int length, void *private_data)
 {
 	// XXX: only 4B aligned writes are allowed here
 	if ((addr & 0x3) || (length != 4)) {
@@ -243,7 +241,7 @@ void config0_write_cb(uint64_t addr, const void *data, int length, void *private
 		syslog(LOG_NOTICE,  "Unhandled config0 write to 0x%lx\n", addr);
 }
 
-void server_client_accept(struct warppipe_client_t *client, void *private_data)
+static void server_client_accept(struct warppipe_client_t *client, void *private_data)
 {
 	warppipe_register_config0_read_cb(client, config0_read_cb);
 	warppipe_register_config0_write_cb(client, config0_write_cb);
